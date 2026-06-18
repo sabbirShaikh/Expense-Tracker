b04307cdc07d66895b7a92247cc1869350a83797
Sabbir-tablesprint <Sabbir@tablesprint.com>
2026-06-18 23:23:25 +0530

    Deploy using Vercel Zero Config and move server to api folder

--- api/index.js
+++ api/index.js
@@ -493,8 +493,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
-app.listen(PORT, () => {
-  console.log(`Secure API Proxy Server running locally on http://localhost:${PORT}`)
-})
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Secure API Proxy Server running locally on http://localhost:${PORT}`)
+  })
+}
 
 export default app
